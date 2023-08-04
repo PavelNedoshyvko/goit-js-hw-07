b742852922468a52cffc22ff08776077a29bbf10
Pavel Nedoshyvko <plight83@gmail.com>
2023-08-04 14:14:54 +0300

    added 02-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,12 +15,12 @@ createMarkup(galleryItems);
 function createMarkup(gallery) {
 	const markupString = galleryItems.map(({ preview, original, description }) => 
 `<li class="gallery__item">
-  <a class="gallery__link" href= ${original}>
+  <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
-      src= ${preview}
-      data-source= ${original}
-      alt= ${description}
+      src=${preview}
+      data-source=${original}
+      alt=${description}
     />
   </a>
 </li>`).join('');
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+	container: document.querySelector('.gallery'),
+};
+
+createMarkup(galleryItems);
+
+function createMarkup(gallery) {
+	const markupString = galleryItems.map(({ preview, original, description }) => 
+`<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} />
+   </a>
+</li>`).join('');
+	
+	refs.container.insertAdjacentHTML('beforeend', markupString);
+};
+
+new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
